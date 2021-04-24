8ac6e7377c5e266e3b0badd805760ceac05326a7
Jorge Serrano <jserrano@dadep.gov.co>
2021-04-23 21:29:29 -0500

    Feature: Se implementa la interfaz de botones de la calculadora.

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,249 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(
-    ) {
-  runApp(MyAplicacion());
+void main() {
+  runApp(MyApp());
 }
 
-class MyAplicacion extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Calculadora();
+  }
+}
+
+class Calculadora extends StatefulWidget {
+  @override
+  _CalculadoraState createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  String operaciones="0";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Jaserranoj',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.black
+                  )
+              ),
+              child: FittedBox(
+                alignment: AlignmentDirectional.centerEnd,
+                fit: BoxFit.fitHeight,
+                child: Text(
+                    operaciones
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "7"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "8"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "9"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "÷"
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Flexible(child: SizedBox(height: 10,)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "4"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "5"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "6"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "X"
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: (){
+                              setState((){
+                              operaciones += "0";});
+                            },
+                            child: Text(
+                                "1"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "2"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "3"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "-"
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "0"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "."
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){},
+                            child: Text(
+                                "+"
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        ElevatedButton(
+                            onPressed: (){
+                              operaciones="0";
+                              setState(() {});
+                            },
+                            child: Text(
+                                "C"
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  height: 50,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: ElevatedButton(
+                      child: Text('='),
+                      onPressed: (){
+                      },
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
-      home: HomePage(),
     );
   }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
- }
-
-
-class _HomePageState extends State<HomePage> {
-  int _counter=0;
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Inicio"),
-      ),
-      body: Center(
-        child: Text("Valor $_counter"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:_botonFlotante,
-        child: Icon(Icons.access_alarms),
-      ),
-
-    );
-  }
-  _botonFlotante() {
-    setState((){
-      _counter++;
-
-    });
-}
-
 }
